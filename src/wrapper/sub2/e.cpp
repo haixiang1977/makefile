41b103978e66c4c8adbded9a7f8655d9688d2d4a
liu haixiang <haixiang1977@yahoo.com>
2017-02-18 10:47:09 +0800

    add sub2 directory
    Signed-off-by: liu haixiang <haixiang1977@yahoo.com>

---
+++ src/wrapper/sub2/e.cpp
@@ -0,0 +1,7 @@
+#include "iostream"
+#include "e.h"
+
+void E::print() {
+    std::cout << "E Hello world" << std::endl;
+    return;
+}
---
+++ src/wrapper/sub2/e.h
@@ -0,0 +1,10 @@
+#ifndef _E_H_
+#define _E_H_
+
+class E {
+public:
+    void print();
+};
+
+#endif
+
