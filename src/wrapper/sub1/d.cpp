0893ce16973de354c0d89abd6d2b7e9a89b91067
liu haixiang <haixiang1977@yahoo.com>
2017-02-18 10:50:09 +0800

    add sub2 build
    Signed-off-by: liu haixiang <haixiang1977@yahoo.com>

--- src/wrapper/sub1/d.cpp
+++ src/wrapper/sub1/d.cpp
@@ -1,7 +1,11 @@
 #include "iostream"
 #include "d.h"
+#include "e.h"
 
 void D::print() {
     std::cout << "D Hello world" << std::endl;
+    E* e = new E();
+    e->print();
+    delete e;
     return;
 }
